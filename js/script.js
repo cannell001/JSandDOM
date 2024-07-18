28d355ac7a38106e91debed09eb938de3ff3ccd9
cannell001 <cannell001@gannon.com>
2024-07-18 17:51:05 -0400

    JavaScript Intro

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log(`I like pizza!`)
+
+console.dir(document.body.style)
+
+document.body.style.backgroundColor = 'hsl(0,0%,15%)'
+document.body.style.color = 'white'
+
+window.alert(`This is an alert!`)
+
+window.alert(`I like pizza!`)
+
+document.getElementById('myH1').textContent = `Hello`
+
+document.getElementById('myP').textContent = `I like pizza!`
+
+// This is a comment
+
+/*
+    This
+    is
+    a
+    comment
+*/
