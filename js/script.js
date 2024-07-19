334af5acf083581f866c655aee26a526a0906ab1
cannell001 <cannell001@gannon.com>
2024-07-19 07:20:09 -0400

    Dynamic Square

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
-// How to accept user input
+document.getElementById('createSquare').addEventListener('click', function () {
+    const square = document.createElement('div')
+    square.id = 'square'
+    square.style.width = '400px'
+    square.style.height = '400px'
+    square.style.backgroundColor = '#f0f0f0' // Initial color
+    document.body.appendChild(square)
 
-// 1. EASY WAY = window prompt
-// 2. PROFESSIONAL WAY = HTML textbox
+    document.getElementById('colorSquare').disabled = false
+})
 
-// ------------------------ EASY WAY ------------------------
-// let username = window.prompt("What's your username?")
-
-// console.log(username)
-
-// -------------------- PROFESSIONAL WAY --------------------
-// let username
-
-// document.getElementById('mySubmit').onclick = function () {
-//     username = document.getElementById('myText').value
-//     document.getElementById('myH1').textContent = `Hello ${username}`
-// }
-
-const mySubmit = document.getElementById('mySubmit')
-const myH1 = document.getElementById('myH1')
-const myText = document.getElementById('myText')
-
-// mySubmit.addEventListener('click', function () {
-//     const username = myText('myText').value
-//     myH1.textContent = `Hello ${username}`
-// })
-
-mySubmit.addEventListener('click', () => {
-    const username = myText.value
-    myH1.textContent = `Hello ${username}`
+document.getElementById('colorSquare').addEventListener('click', function () {
+    const square = document.getElementById('square')
+    if (square) {
+        square.style.backgroundColor = 'red'
+    }
 })
